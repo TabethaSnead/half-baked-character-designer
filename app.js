2aaa5e141052f647f5d6d0cfe9bdaf42b993164e
TabethaSnead <tabetha.snead@gmail.com>
2023-01-11 18:00:18 -0800

    fixed some ids that were swapped, changed url path

--- app.js
+++ app.js
@@ -12,70 +12,76 @@ const catchphrasesEl = document.getElementById('catchphrases');
 const catchphraseInput = document.getElementById('catchphrase-input');
 const catchphraseButton = document.getElementById('catchphrase-button');
 
-const headImg = document.getElementById('headImg');
-const middleImg = document.getElementById('middleImg');
-const pantsImg = document.getElementById('pantsImg');
-
 // set state for how many times the user changes the head, middle, and bottom
 let headCount = 0;
 let middleCount = 0;
 let bottomCount = 0;
-let catchphrases = [];
+const catchphrases = [];
 // set state for all of the character's catchphrases
 
 headDropdown.addEventListener('change', () => {
-    headImg.src = `../assets/${headDropdown.value}-head.png`;
+    headEl.style.backgroundImage = `url(./assets/${headDropdown.value}-head.png)`;
 
     // get the value of the head dropdown
 
     // increment the head change count state
     headCount++;
-    displayStats();
-    console.log(headCount);
+
     // update the dom for the head (use style.backgroundImage on the headEl div instead of trying to set the .src -- it's NOT an img tag!)
 
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 middleDropdown.addEventListener('change', () => {
-    middleImg.src = `../assets/${middleDropdown.value}-middle.png`;
+    middleEl.style.backgroundImage = `url(./assets/${middleDropdown.value}-middle.png)`;
     // get the value of the middle dropdown
     // increment the middle change count state
     middleCount++;
-    displayStats();
     // update the dom for the middle (NOTE: use style.backgroundImage on the middleEl div instead of trying to set the .src -- it's NOT an img tag!)
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 bottomDropdown.addEventListener('change', () => {
+    // let bttmDropdownval = bottomDropdown.value;
     // get the value of the bottom dropdown
-    pantsImg.src = `../assets/${bottomDropdown.value}-pants.png`;
+    bottomEl.style.backgroundImage = `url(./assets/${bottomDropdown.value}-pants.png)`;
     // increment the bottom change count state
     bottomCount++;
-    displayStats();
     // update the dom for the bottom (NOTE use style.backgroundImage on the bottomEl div instead of trying to set the .src -- it's NOT an img tag!)
     // update the stats to show the new count (call displayStats() to do this work)
+    displayStats();
 });
 
 catchphraseButton.addEventListener('click', () => {
     // get the value of the catchphrase input
-    catchphrases.push(catchphraseInput.value);
-    console.log(catchphrases);
-    catchphrasesEl.textContent = '';
+    const inputPhrase = catchphraseInput.value;
     // push the new catchphrase to the catchphrase array in state
+    catchphrases.push(inputPhrase);
     // clear out the form input's value so it's empty to the user
+    catchphraseInput.value = '';
     // update the dom to show the new catchphrases (refactor to/call displayCatchphrases to do this work)
+    displayCatchPhrases();
 });
 
 function displayStats() {
-    headEl.textContent = headCount;
-    middleEl.textContent = middleCount;
-    bottomEl.textContent = bottomCount;
+    reportEl.textContent = `you have changed the head ${headCount} times. You have changed the middle ${middleCount} times.
+    you have changed the pants ${bottomCount} times.`;
+
     // text content of the reportEl to tell the user how many times they've changed each piece of the state
 }
 
-function displayCatchphrases() {
+function displayCatchPhrases() {
     // clear out the DOM for the currently displayed catchphrases
+    catchphrasesEl.textContent = '';
+
     // loop through each catchphrase in state
+    for (let phrase of catchphrases) {
+        const displayPhrase = document.createElement('div');
+        displayPhrase.classList.add('catchPhrase-class');
+        displayPhrase.textContent = phrase;
+        catchphrasesEl.append(displayPhrase);
+    }
     // and for each catchphrase
 }
